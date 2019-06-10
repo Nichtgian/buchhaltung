8bde30a4ad7b19fd4c3a1efd94e4894c246ced7b
Nychtgian <nichtgian@gmail.com>
2019-06-10 13:09:30 +0200

    Created default

---
+++ Buchhaltung/Buchhaltung/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace Buchhaltung.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Bilanz";
+            return View();
+        }
+
+        public ActionResult Journal()
+        {
+            ViewBag.Title = "Journal";
+            return View();
+        }
+
+        public ActionResult Start()
+        {
+            ViewBag.Title = "Anfangsbeträge";
+            return View();
+        }
+    }
+}
---
+++ Buchhaltung/Buchhaltung/Models/KontoRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Buchhaltung.Models
+{
+    public class KontoRepository
+    {
+        public IEnumerable<Konto> GetAllKontos()
+        {
+            using (var ctx = new BuchhaltungDbContext())
+            {
+                return ctx.Konto.ToList();
+            }
+        }
+    }
+}
